3b0b7d09de035189eb6e9647b9728af082780a7f
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2022-06-14 22:42:27 +0900

    問題2のヒント追加

--- question_1/app.js
+++ question_1/app.js
@@ -2,3 +2,7 @@ window.onload = ()=>{
     const loader = document.getElementById('loader');
     loader.classList.add('loaded');
 }
+
+const decrypt_hint = () =>{
+    alert('データを64種類の印字可能な英数字のみを用いたエンコード方式')
+}
